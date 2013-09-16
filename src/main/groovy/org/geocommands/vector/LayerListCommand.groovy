a3d72f8db603e1a78671e2d43142c83214e725de
Jared Erickson <jared.erickson@gmail.com>
2013-09-16 14:02:03 -0700

    Add layer list command

---
+++ src/main/groovy/org/geocommands/vector/LayerListCommand.groovy
@@ -0,0 +1,48 @@
+package org.geocommands.vector
+
+import geoscript.workspace.Workspace
+import org.geocommands.Command
+import org.geocommands.Options
+import org.geotools.data.DataStoreFinder
+import org.kohsuke.args4j.Option
+
+/**
+ * List Layers in a DataStore
+ * @author Jared Erickson
+ */
+class LayerListCommand extends Command<LayerListOptions>{
+
+    @Override
+    String getName() {
+        "vector list layers"
+    }
+
+    @Override
+    String getDescription() {
+        "List Layers in a DataStore"
+    }
+
+    @Override
+    LayerListOptions getOptions() {
+        new LayerListOptions()
+    }
+
+    @Override
+    void execute(LayerListOptions options, Reader reader, Writer writer) throws Exception {
+        Workspace workspace = new Workspace(options.inputWorkspace)
+        String NEW_LINE = System.getProperty("line.separator")
+        StringBuilder builder = new StringBuilder()
+        workspace.names.eachWithIndex{String name, int i ->
+            if (i > 0) {
+                builder.append(NEW_LINE)
+            }
+            builder.append(name)
+        }
+        writer.write(builder.toString())
+    }
+
+    static class LayerListOptions extends Options {
+        @Option(name="-i", aliases="--input-workspace",  usage="The input workspace", required = false)
+        String inputWorkspace
+    }
+}
---
+++ src/test/groovy/org/geocommands/vector/LayerListCommandTest.groovy
@@ -0,0 +1,33 @@
+package org.geocommands.vector
+
+import org.geocommands.vector.LayerListCommand.LayerListOptions
+import org.geocommands.BaseTest
+import org.junit.Test
+
+/**
+ * The LayerListCommand Unit Test
+ * @author Jared Erickson
+ */
+class LayerListCommandTest extends BaseTest {
+
+    @Test void execute() {
+        LayerListCommand cmd = new LayerListCommand()
+        LayerListOptions options = new LayerListOptions(
+            inputWorkspace: "dbtype=h2 database=src/test/resources/h2.db"
+        )
+        StringWriter writer = new StringWriter()
+        cmd.execute(options, new StringReader(""), writer)
+        assertStringsEqual("""points
+polygons""", writer.toString())
+    }
+
+    @Test void run() {
+        String result = runApp([
+                "vector list layers",
+                "-i", "dbtype=h2 database=src/test/resources/h2.db"
+        ],"")
+        assertStringsEqual("""points
+polygons""", result)
+    }
+
+}

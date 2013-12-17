47d6041cb81fdf857e7d5ee342ec2c353d008180
Jared Erickson <jared.erickson@gmail.com>
2013-12-16 19:25:40 -0800

    Add a geoc geometry convert command for converting geometry between different formats

---
+++ src/main/groovy/org/geocommands/geometry/ConvertCommand.groovy
@@ -0,0 +1,116 @@
+package org.geocommands.geometry
+
+import geoscript.feature.Feature
+import geoscript.feature.Field
+import geoscript.feature.Schema
+import geoscript.geom.Geometry
+import geoscript.layer.Layer
+import geoscript.workspace.Memory
+import org.geocommands.Command
+import org.geocommands.Options
+import org.kohsuke.args4j.Option
+
+/**
+ * Convert a Geometry from one format to another.
+ * @author Jared Erickson
+ */
+class ConvertCommand extends Command<ConvertOptions> {
+
+    @Override
+    String getName() {
+        "geometry convert"
+    }
+
+    @Override
+    String getDescription() {
+        "Convert a geometry from one format to another"
+    }
+
+    @Override
+    ConvertOptions getOptions() {
+        new ConvertOptions()
+    }
+
+    @Override
+    void execute(ConvertOptions options, Reader reader, Writer writer) throws Exception {
+        Geometry geom = Geometry.fromString(options.input ? options.input : reader.text)
+        if (options.type.equalsIgnoreCase("geometry")) {
+            geoscript.geom.io.Writer geometryWriter = getGeometryWriter(options.format, options.formatOptions)
+            writer.write(geometryWriter.write(geom))
+        } else if (options.type.equalsIgnoreCase("feature")) {
+            Schema schema = new Schema("feature", new Field("geom", geom.geometryType))
+            Feature feature = schema.feature([geom: geom], "1")
+            geoscript.feature.io.Writer featureWriter = getFeatureWriter(options.format, options.formatOptions)
+            writer.write(featureWriter.write(feature))
+        } else if (options.type.equalsIgnoreCase("layer")) {
+            Schema schema = new Schema("feature", new Field("geom", geom.geometryType))
+            Layer layer = new Memory().create(schema)
+            Feature feature = schema.feature([geom: geom], "1")
+            layer.add(feature)
+            geoscript.layer.io.Writer layerWriter = getLayerWriter(options.format, options.formatOptions)
+            writer.write(layerWriter.write(layer))
+        } else {
+            throw new IllegalArgumentException("Unknown output type '${options.type}'. Please use geometry, feature, or layer!")
+        }
+    }
+
+    private geoscript.geom.io.Writer getGeometryWriter(String format, Map formatOptions) {
+        if (format.equalsIgnoreCase("wkt")) {
+            new geoscript.geom.io.WktWriter()
+        } else if (format.equalsIgnoreCase("geojson")) {
+            new geoscript.geom.io.GeoJSONWriter()
+        } else if (format.equalsIgnoreCase("georss")) {
+            new geoscript.geom.io.GeoRSSWriter(options.formatOptions)
+        } else if (format.equalsIgnoreCase("gml2") || format.equalsIgnoreCase("gml")) {
+            new geoscript.geom.io.Gml2Writer()
+        } else if (format.equalsIgnoreCase("gml3")) {
+            new geoscript.geom.io.Gml3Writer()
+        } else if (format.equalsIgnoreCase("kml")) {
+            new geoscript.geom.io.KmlWriter()
+        } else if (format.equalsIgnoreCase("wkb")) {
+            new geoscript.geom.io.WkbWriter()
+        } else {
+            throw new IllegalArgumentException("Unknown format '${format}'!")
+        }
+    }
+
+    private geoscript.feature.io.Writer getFeatureWriter(String format, Map formatOptions) {
+        if (format.equalsIgnoreCase("geojson")) {
+            new geoscript.feature.io.GeoJSONWriter()
+        } else if (format.equalsIgnoreCase("gml")) {
+            new geoscript.feature.io.GmlWriter()
+        } else {
+            throw new IllegalArgumentException("Unknown format '${format}'!")
+        }
+    }
+
+    private geoscript.layer.io.Writer getLayerWriter(String format, Map formatOptions) {
+        if (format.equalsIgnoreCase("csv")) {
+            new geoscript.layer.io.CsvWriter()
+        } else if (format.equalsIgnoreCase("geojson")) {
+            new geoscript.layer.io.GeoJSONWriter()
+        } else if (format.equalsIgnoreCase("gml")) {
+            new geoscript.layer.io.GmlWriter()
+        } else if (format.equalsIgnoreCase("kml")) {
+            new geoscript.layer.io.KmlWriter()
+        } else {
+            throw new IllegalArgumentException("Unknown format '${format}'!")
+        }
+    }
+
+    static class ConvertOptions extends Options {
+
+        @Option(name = "-i", aliases = "--input", usage = "The input geometry", required = false)
+        String input
+
+        @Option(name = "-f", aliases = "--format", usage = "The output format", required = true)
+        String format
+
+        @Option(name = "-p", aliases = "--format-options", usage = "The output format options", required = false)
+        Map<String, String> formatOptions
+
+        @Option(name = "-t", aliases = "--type", usage = "The output type (geometry, feature, layer)", required = false)
+        String type = "geometry"
+
+    }
+}
---
+++ src/test/groovy/org/geocommands/geometry/ConvertCommandTest.groovy
@@ -0,0 +1,106 @@
+package org.geocommands.geometry
+
+import groovy.json.JsonSlurper
+import org.geocommands.BaseTest
+import org.geocommands.geometry.ConvertCommand.ConvertOptions
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+
+/**
+ * The ConvertCommand Unit Test
+ * @author Jared Erickson
+ */
+class ConvertCommandTest extends BaseTest {
+
+    @Test
+    void executeWktToGeoJSONGeometryFromReader() {
+        ConvertCommand cmd = new ConvertCommand()
+        ConvertOptions options = new ConvertOptions(
+                format: "geojson"
+        )
+        StringReader reader = new StringReader("POINT (1 2)")
+        StringWriter writer = new StringWriter()
+        cmd.execute(options, reader, writer)
+        assertEquals("{ \"type\": \"Point\", \"coordinates\": [1.0, 2.0] }", writer.toString())
+    }
+
+    @Test
+    void executeWktToGeoJSONGeometryFromInput() {
+        ConvertCommand cmd = new ConvertCommand()
+        ConvertOptions options = new ConvertOptions(
+                input: "POINT (1 2)",
+                format: "geojson"
+        )
+        StringReader reader = new StringReader("")
+        StringWriter writer = new StringWriter()
+        cmd.execute(options, reader, writer)
+        assertEquals("{ \"type\": \"Point\", \"coordinates\": [1.0, 2.0] }", writer.toString())
+    }
+
+    @Test
+    void executeWktToGeoJSONFeatureFromReader() {
+        ConvertCommand cmd = new ConvertCommand()
+        ConvertOptions options = new ConvertOptions(
+                format: "geojson",
+                type: "feature"
+        )
+        StringReader reader = new StringReader("POINT (1 2)")
+        StringWriter writer = new StringWriter()
+        cmd.execute(options, reader, writer)
+        assertEquals("{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[1,2]},\"properties\":{},\"id\":\"1\"}", writer.toString())
+    }
+
+    @Test
+    void executeWktToGeoJSONLayerFromReader() {
+        ConvertCommand cmd = new ConvertCommand()
+        ConvertOptions options = new ConvertOptions(
+                format: "geojson",
+                type: "layer"
+        )
+        StringReader reader = new StringReader("POINT (1 2)")
+        StringWriter writer = new StringWriter()
+        cmd.execute(options, reader, writer)
+        def json = new JsonSlurper().parseText(writer.toString())
+        assertEquals "FeatureCollection", json.type
+        assertEquals 1, json.features.size()
+        assertEquals "Feature", json.features[0].type
+        assertEquals "Point", json.features[0].geometry.type
+        assertEquals 1, json.features[0].geometry.coordinates[0]
+        assertEquals 2, json.features[0].geometry.coordinates[1]
+        assertNotNull json.features[0].properties
+        assertNotNull json.features[0].id
+    }
+
+    @Test
+    void runWktToKmlGeometry() {
+        String actual = runApp([
+                "geometry convert",
+                "-f", "kml"
+        ], "POINT (1 2)").trim()
+        String expected = "<Point><coordinates>1.0,2.0</coordinates></Point>"
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    void runWktToGmlFeature() {
+        String actual = runApp([
+                "geometry convert",
+                "-i", "<Point><coordinates>1.0,2.0</coordinates></Point>",
+                "-f", "gml",
+                "-t", "feature"
+        ], "").trim()
+        String expected = """<gsf:feature xmlns:gsf="http://geoscript.org/feature" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:gml="http://www.opengis.net/gml" fid="1">
+<gsf:geom>
+<gml:Point>
+<gml:coord>
+<gml:X>1.0</gml:X>
+<gml:Y>2.0</gml:Y>
+</gml:coord>
+</gml:Point>
+</gsf:geom>
+</gsf:feature>"""
+        assertStringsEqual(expected, actual, true)
+    }
+}

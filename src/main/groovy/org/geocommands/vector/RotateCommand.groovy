d63fbdc0ff9bef66bd21df7b13539e69622e5c12
Jared Erickson <jared.erickson@gmail.com>
2013-12-23 18:07:33 -0800

    Add geoc vector rotate command

---
+++ src/main/groovy/org/geocommands/vector/RotateCommand.groovy
@@ -0,0 +1,114 @@
+package org.geocommands.vector
+
+import geoscript.feature.Feature
+import geoscript.filter.Expression
+import geoscript.layer.Layer
+import org.kohsuke.args4j.Option
+
+/**
+ * Rotate Features in a Layer.
+ * @author Jared Erickson
+ */
+class RotateCommand extends LayerInOutCommand<RotateOptions>{
+
+    @Override
+    String getName() {
+        "vector rotate"
+    }
+
+    @Override
+    String getDescription() {
+        "Rotate Features in a Layer"
+    }
+
+    @Override
+    RotateOptions getOptions() {
+        new RotateOptions()
+    }
+
+    @Override
+    void processLayers(Layer inLayer, Layer outLayer, RotateOptions options, Reader reader, Writer writer) throws Exception {
+        Closure c
+        // theta
+        if (options.theta) {
+            // theta x y
+            if (options.x && options.y) {
+                Expression thetaExpression = Expression.fromCQL(options.theta)
+                Expression xExpression = Expression.fromCQL(options.x)
+                Expression yExpression = Expression.fromCQL(options.y)
+                c = {geoscript.layer.Writer w, Feature f ->
+                    double theta = thetaExpression.evaluate(f)
+                    double x = xExpression.evaluate(f)
+                    double y = yExpression.evaluate(f)
+                    f.geom = f.geom.rotate(theta, x, y)
+                    w.add(f)
+                }
+            }
+            // theta
+            else {
+                Expression thetaExpression = Expression.fromCQL(options.theta)
+                c = {geoscript.layer.Writer w, Feature f ->
+                    double theta = thetaExpression.evaluate(f)
+                    f.geom = f.geom.rotate(theta)
+                    w.add(f)
+                }
+            }
+        }
+        // sin and cos
+        else if (options.sin && options.cos) {
+            // sin cos x y
+            if (options.x && options.y) {
+                Expression sinExpression = Expression.fromCQL(options.sin)
+                Expression cosExpression = Expression.fromCQL(options.cos)
+                Expression xExpression = Expression.fromCQL(options.x)
+                Expression yExpression = Expression.fromCQL(options.y)
+                c = {geoscript.layer.Writer w, Feature f ->
+                    double sin = sinExpression.evaluate(f)
+                    double cos = cosExpression.evaluate(f)
+                    double x = xExpression.evaluate(f)
+                    double y = yExpression.evaluate(f)
+                    f.geom = f.geom.rotate(sin, cos, x, y)
+                    w.add(f)
+                }
+            }
+            // sin and cos
+            else {
+                Expression sinExpression = Expression.fromCQL(options.sin)
+                Expression cosExpression = Expression.fromCQL(options.cos)
+                c = {geoscript.layer.Writer w, Feature f ->
+                    double sin = sinExpression.evaluate(f)
+                    double cos = cosExpression.evaluate(f)
+                    f.geom = f.geom.rotate(sin, cos)
+                    w.add(f)
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Illegal combination of arguments! theta or theta, x, y or sin,cos,x,y or sin,cos.")
+        }
+
+        outLayer.withWriter {geoscript.layer.Writer w ->
+            inLayer.cursor.collect{Feature f ->
+                c.call(w,f)
+            }
+        }
+    }
+
+    static class RotateOptions extends LayerInOutOptions {
+
+        @Option(name="-t", aliases="--theta",  usage="The angle of rotation in radians", required = false)
+        String theta
+
+        @Option(name="-s", aliases="--sine",  usage="The sine of the angle of rotation in radians", required = false)
+        String sin
+
+        @Option(name="-c", aliases="--cosine",  usage="The cosine of the angle of rotation in radians", required = false)
+        String cos
+
+        @Option(name="-x", aliases="--x-coord",  usage="The x coordinate of the rotation point", required = false)
+        String x
+
+        @Option(name="-y", aliases="--y-coord",  usage="The y coordinate of the rotation point", required = false)
+        String y
+
+    }
+}
---
+++ src/test/groovy/org/geocommands/vector/RotateCommandTest.groovy
@@ -0,0 +1,199 @@
+package org.geocommands.vector
+
+import geoscript.feature.Feature
+import geoscript.filter.Expression
+import geoscript.geom.Geometry
+import geoscript.layer.Layer
+import geoscript.layer.Property
+import geoscript.layer.Shapefile
+import geoscript.layer.io.CsvReader
+import org.geocommands.BaseTest
+import org.geocommands.vector.RotateCommand.RotateOptions
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+/**
+ * The RotateCommand Unit Test
+ * @author Jared Erickson
+ */
+class RotateCommandTest extends BaseTest {
+
+    @Test
+    void executeThetaWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        RotateCommand cmd = new RotateCommand()
+        RotateOptions options = new RotateOptions(
+                inputWorkspace: inFile.absolutePath,
+                outputWorkspace: outFile,
+                theta: com.vividsolutions.jts.algorithm.Angle.toRadians(45)
+        )
+        cmd.execute(options, new StringReader(""), new StringWriter())
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(com.vividsolutions.jts.algorithm.Angle.toRadians(45)), outGeom
+        }
+    }
+
+    @Test
+    void executeThetaXYWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        RotateCommand cmd = new RotateCommand()
+        RotateOptions options = new RotateOptions(
+                inputWorkspace: inFile.absolutePath,
+                outputWorkspace: outFile,
+                theta: com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                x: "getX(centroid(geom))",
+                y: "getY(centroid(geom))"
+        )
+        cmd.execute(options, new StringReader(""), new StringWriter())
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                    inGeom.centroid.x, inGeom.centroid.y), outGeom
+        }
+    }
+
+    @Test
+    void executeSineCosineWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        RotateCommand cmd = new RotateCommand()
+        RotateOptions options = new RotateOptions(
+                inputWorkspace: inFile.absolutePath,
+                outputWorkspace: outFile,
+                sin: com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                cos: com.vividsolutions.jts.algorithm.Angle.toRadians(15),
+        )
+        cmd.execute(options, new StringReader(""), new StringWriter())
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(
+                    com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                    com.vividsolutions.jts.algorithm.Angle.toRadians(15)), outGeom
+        }
+    }
+
+    @Test
+    void executeSineCosineXYWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        RotateCommand cmd = new RotateCommand()
+        RotateOptions options = new RotateOptions(
+                inputWorkspace: inFile.absolutePath,
+                outputWorkspace: outFile,
+                sin: com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                cos: com.vividsolutions.jts.algorithm.Angle.toRadians(15),
+                x: "getX(centroid(geom))",
+                y: "getY(centroid(geom))"
+        )
+        cmd.execute(options, new StringReader(""), new StringWriter())
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(
+                    com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                    com.vividsolutions.jts.algorithm.Angle.toRadians(15),
+                    inGeom.centroid.x, inGeom.centroid.y), outGeom
+        }
+    }
+
+
+    @Test
+    void executeWithText() {
+        StringReader reader = getStringReader("polys.csv")
+        StringWriter writer = new StringWriter()
+        RotateCommand cmd = new RotateCommand()
+        RotateOptions options = new RotateOptions(
+                theta: com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                x: "getX(centroid(geom))",
+                y: "getY(centroid(geom))"
+        )
+        cmd.execute(options, reader, writer)
+        CsvReader csvReader = new CsvReader()
+        Layer inLayer = csvReader.read(getStringReader("polys.csv").text)
+        Layer outLayer = csvReader.read(writer.toString())
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                    inGeom.centroid.x, inGeom.centroid.y), outGeom
+        }
+    }
+
+    @Test
+    void runWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        runApp([
+                "vector rotate",
+                "-i", inFile.absolutePath,
+                "-o", outFile.absolutePath,
+                "-t", com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                "-x", "getX(centroid(geom))",
+                "-y", "getY(centroid(geom))"
+        ], "")
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                    inGeom.centroid.x, inGeom.centroid.y), outGeom
+        }
+    }
+
+    @Test
+    void runWithText() {
+        StringReader reader = getStringReader("polys.csv")
+        String result = runApp([
+                "vector Rotate",
+                "-t", com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                "-x", "getX(centroid(geom))",
+                "-y", "getY(centroid(geom))"
+        ], reader.text)
+        CsvReader csvReader = new CsvReader()
+        Layer inLayer = csvReader.read(getStringReader("polys.csv").text)
+        Layer outLayer = csvReader.read(result)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.rotate(com.vividsolutions.jts.algorithm.Angle.toRadians(45),
+                    inGeom.centroid.x, inGeom.centroid.y), outGeom
+        }
+    }
+}

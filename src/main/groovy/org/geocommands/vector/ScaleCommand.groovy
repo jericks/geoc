350d8b599ab9134611fa8af6f53ff4ffd4326537
Jared Erickson <jared.erickson@gmail.com>
2013-12-24 16:54:16 -0800

    Add geoc vector scale command

---
+++ src/main/groovy/org/geocommands/vector/ScaleCommand.groovy
@@ -0,0 +1,66 @@
+package org.geocommands.vector
+
+import geoscript.feature.Feature
+import geoscript.filter.Expression
+import geoscript.layer.Layer
+import org.kohsuke.args4j.Option
+
+/**
+ * Scale Features in a Layer.
+ * @author Jared Erickson
+ */
+class ScaleCommand extends LayerInOutCommand<ScaleOptions>{
+
+    @Override
+    String getName() {
+        "vector scale"
+    }
+
+    @Override
+    String getDescription() {
+        "Scale Feature in a Layer"
+    }
+
+    @Override
+    ScaleOptions getOptions() {
+        new ScaleOptions()
+    }
+
+    @Override
+    void processLayers(Layer inLayer, Layer outLayer, ScaleOptions options, Reader reader, Writer writer) throws Exception {
+        Expression xDistanceExpression = Expression.fromCQL(options.xDistance)
+        Expression yDistanceExpression = Expression.fromCQL(options.yDistance)
+        Expression xCoordExpression = options.xCoord ? Expression.fromCQL(options.xCoord) : null
+        Expression yCoordExpression = options.yCoord ? Expression.fromCQL(options.yCoord) : null
+        outLayer.withWriter {geoscript.layer.Writer w ->
+            inLayer.cursor.collect{Feature f ->
+                double xDistance = xDistanceExpression.evaluate(f) as double
+                double yDistance = yDistanceExpression.evaluate(f) as double
+                if (xCoordExpression && yCoordExpression) {
+                    double xCoord = xCoordExpression.evaluate(f) as double
+                    double yCoord = yCoordExpression.evaluate(f) as double
+                    f.geom = f.geom.scale(xDistance, yDistance, xCoord, yCoord)
+                } else {
+                    f.geom = f.geom.scale(xDistance, yDistance)
+                }
+                w.add(f)
+            }
+        }
+    }
+
+    static class ScaleOptions extends LayerInOutOptions {
+
+        @Option(name="-x", aliases="--x-distance",  usage="The x distance", required = true)
+        String xDistance
+
+        @Option(name="-y", aliases="--y-distance",  usage="The y distance", required = true)
+        String yDistance
+
+        @Option(name="-c", aliases="--x-coord",  usage="The x coordinate", required = false)
+        String xCoord
+
+        @Option(name="-d", aliases="--y-coord",  usage="The y coordinate", required = false)
+        String yCoord
+
+    }
+}
---
+++ src/test/groovy/org/geocommands/vector/ScaleCommandTest.groovy
@@ -0,0 +1,122 @@
+package org.geocommands.vector
+
+import geoscript.feature.Feature
+import geoscript.filter.Expression
+import geoscript.geom.Geometry
+import geoscript.layer.Layer
+import geoscript.layer.Property
+import geoscript.layer.Shapefile
+import geoscript.layer.io.CsvReader
+import org.geocommands.BaseTest
+import org.geocommands.vector.ScaleCommand.ScaleOptions
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+
+/**
+ * The ScaleCommand Unit Test
+ * @author Jared Erickson
+ */
+class ScaleCommandTest extends BaseTest {
+
+    @Test
+    void executeWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        ScaleCommand cmd = new ScaleCommand()
+        ScaleOptions options = new ScaleOptions(
+                inputWorkspace: inFile.absolutePath,
+                outputWorkspace: outFile,
+                xDistance: 5,
+                yDistance: 4
+        )
+        cmd.execute(options, new StringReader(""), new StringWriter())
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.scale(5, 4), outGeom
+        }
+    }
+
+    @Test
+    void executeWithText() {
+        StringReader reader = getStringReader("polys.csv")
+        StringWriter writer = new StringWriter()
+        ScaleCommand cmd = new ScaleCommand()
+        ScaleOptions options = new ScaleOptions(
+                xDistance: "parseDouble(id)",
+                yDistance: "parseDouble(id) * 2",
+                xCoord: "getX(centroid(geom))",
+                yCoord: "getY(centroid(geom))"
+        )
+        cmd.execute(options, reader, writer)
+        CsvReader csvReader = new CsvReader()
+        Layer inLayer = csvReader.read(getStringReader("polys.csv").text)
+        Layer outLayer = csvReader.read(writer.toString())
+        assertEquals inLayer.count, outLayer.count
+        List inFeatures = inLayer.features
+        List outFeatures = outLayer.features
+        (0..<inFeatures.size()).each { i ->
+            Feature inFeature = inFeatures[i]
+            Feature outFeature = outFeatures[i]
+            double xDist = Expression.fromCQL("parseDouble(id)").evaluate(inFeature) as double
+            double yDist = Expression.fromCQL("parseDouble(id) * 2").evaluate(inFeature) as double
+            double xCoord = Expression.fromCQL("getX(centroid(geom))").evaluate(inFeature) as double
+            double yCoord = Expression.fromCQL("getY(centroid(geom))").evaluate(inFeature) as double
+            assertEquals inFeature.geom.scale(xDist, yDist, xCoord, yCoord), outFeature.geom
+        }
+    }
+
+    @Test
+    void runWithFiles() {
+        File inFile = getResource("polys.properties")
+        File outFile = createTemporaryShapefile("polys")
+        runApp([
+                "vector scale",
+                "-i", inFile.absolutePath,
+                "-o", outFile.absolutePath,
+                "-x", "5",
+                "-y", "5",
+                "-c", "getX(centroid(geom))",
+                "-d", "getY(centroid(geom))",
+        ], "")
+        Layer inLayer = new Property(inFile)
+        Layer outLayer = new Shapefile(outFile)
+        assertEquals inLayer.count, outLayer.count
+        List inGeoms = inLayer.collectFromFeature { it.geom }
+        List outGeoms = outLayer.collectFromFeature { it.geom }
+        (0..<inGeoms.size()).each { i ->
+            Geometry inGeom = inGeoms[i]
+            Geometry outGeom = outGeoms[i]
+            assertEquals inGeom.scale(5, 5, inGeom.centroid.x, inGeom.centroid.y), outGeom.getGeometryN(0)
+        }
+    }
+
+    @Test
+    void runWithText() {
+        StringReader reader = getStringReader("polys.csv")
+        String result = runApp([
+                "vector scale",
+                "-x", "parseDouble(id)",
+                "-y", "parseDouble(id) * 2"
+        ], reader.text)
+        CsvReader csvReader = new CsvReader()
+        Layer inLayer = csvReader.read(getStringReader("polys.csv").text)
+        Layer outLayer = csvReader.read(result)
+        assertEquals inLayer.count, outLayer.count
+        List inFeatures = inLayer.features
+        List outFeatures = outLayer.features
+        (0..<inFeatures.size()).each { i ->
+            Feature inFeature = inFeatures[i]
+            Feature outFeature = outFeatures[i]
+            double x = Expression.fromCQL("parseDouble(id)").evaluate(inFeature) as double
+            double y = Expression.fromCQL("parseDouble(id) * 2").evaluate(inFeature) as double
+            assertEquals inFeature.geom.scale(x, y), outFeature.geom
+        }
+    }
+}

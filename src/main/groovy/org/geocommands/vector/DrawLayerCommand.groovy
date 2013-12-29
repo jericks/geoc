f0674dc6010fc65a1ab760d67189ad0a6adbdc40
Jared Erickson <jared.erickson@gmail.com>
2013-12-29 10:27:48 -0800

    Add sldFile and bounds parameters to the vector draw command

--- src/main/groovy/org/geocommands/vector/DrawLayerCommand.groovy
+++ src/main/groovy/org/geocommands/vector/DrawLayerCommand.groovy
@@ -1,6 +1,9 @@
 package org.geocommands.vector
 
+import geoscript.geom.Bounds
 import geoscript.layer.Layer
+import geoscript.style.io.CSSReader
+import geoscript.style.io.SLDReader
 import org.kohsuke.args4j.Option
 
 /**
@@ -25,28 +28,42 @@ class DrawLayerCommand extends LayerCommand<DrawLayerOptions> {
 
     @Override
     protected void processLayer(Layer layer, DrawLayerOptions options, Reader reader, Writer writer) {
+        if (options.sldFile) {
+            if (options.sldFile.name.endsWith(".sld")) {
+                layer.style = new SLDReader().read(options.sldFile)
+            } else if (options.sldFile.name.endsWith(".css")) {
+                layer.style = new CSSReader().read(options.sldFile)
+            }
+        }
         geoscript.render.Map map = new geoscript.render.Map(
-            layers: [layer],
-            type: options.type,
-            width: options.width,
-            height: options.height
+                layers: [layer],
+                type: options.type,
+                width: options.width,
+                height: options.height,
+                bounds: options.bounds ? Bounds.fromString(options.bounds) : layer.bounds
         )
-        map.render(options.file ? options.file : new File("${['pdf','svg'].contains(options.type) ? "document" : "image"}.${options.type}"))
+        map.render(options.file ? options.file : new File("${['pdf', 'svg'].contains(options.type) ? "document" : "image"}.${options.type}"))
     }
 
     private static class DrawLayerOptions extends LayerOptions {
 
-        @Option(name="-f", aliases="--file",  usage="The output file", required = false)
+        @Option(name = "-f", aliases = "--file", usage = "The output file", required = false)
         File file
 
-        @Option(name="-t", aliases="--type",  usage="The type of document", required = false)
+        @Option(name = "-t", aliases = "--type", usage = "The type of document", required = false)
         String type = "png"
 
-        @Option(name="-w", aliases="--width",  usage="The width", required = false)
+        @Option(name = "-w", aliases = "--width", usage = "The width", required = false)
         int width = 800
 
-        @Option(name="-h", aliases="--height",  usage="The height", required = false)
+        @Option(name = "-h", aliases = "--height", usage = "The height", required = false)
         int height = 600
+
+        @Option(name = "-s", aliases = "--sld-file", usage = "The sld file", required = false)
+        File sldFile
+
+        @Option(name = "-b", aliases = "--bounds", usage = "The bounds", required = false)
+        String bounds
     }
 
 }
--- src/test/groovy/org/geocommands/vector/DrawLayerCommandTest.groovy
+++ src/test/groovy/org/geocommands/vector/DrawLayerCommandTest.groovy
@@ -1,10 +1,12 @@
 package org.geocommands.vector
 
+import geoscript.layer.Property
 import org.geocommands.App
 import org.geocommands.BaseTest
 import org.geocommands.vector.DrawLayerCommand.DrawLayerOptions
 import org.junit.Test
-import static org.junit.Assert.*
+
+import static org.junit.Assert.assertTrue
 
 /**
  * The DrawLayerCommand Unit Test
@@ -12,59 +14,70 @@ import static org.junit.Assert.*
  */
 class DrawLayerCommandTest extends BaseTest {
 
-    @Test void execute() {
+    private File createSldFile(File layerFile) {
+        DefaultStyleCommand cmd = new DefaultStyleCommand()
+        DefaultStyleCommand.DefaultStyleOptions options = new DefaultStyleCommand.DefaultStyleOptions(inputWorkspace: layerFile)
+        String sld = cmd.execute(options)
+        File file = File.createTempFile("style",".sld")
+        file.write(sld)
+        file
+    }
+
+    @Test
+    void execute() {
         DrawLayerCommand cmd = new DrawLayerCommand()
         File file = getResource("polygons.properties")
-        File imgFile = File.createTempFile("polygons_",".png")
-        println imgFile
+        File imgFile = File.createTempFile("polygons_", ".png")
+        File sldFile = createSldFile(file)
         DrawLayerOptions options = new DrawLayerOptions(
-            inputWorkspace: file.absolutePath,
-            width: 400,
-            height: 400,
-            file: imgFile,
-            type: "png"
+                inputWorkspace: file.absolutePath,
+                width: 400,
+                height: 400,
+                file: imgFile,
+                type: "png",
+                sldFile: sldFile
         )
         cmd.execute(options, new StringReader(""), new StringWriter())
         assertTrue imgFile.exists()
     }
 
-    @Test void executeWithCsv() {
+    @Test
+    void executeWithCsv() {
         DrawLayerCommand cmd = new DrawLayerCommand()
-        File imgFile = File.createTempFile("polygons_",".jpeg")
-        println imgFile
+        File imgFile = File.createTempFile("polygons_", ".jpeg")
         DrawLayerOptions options = new DrawLayerOptions(
                 width: 500,
                 height: 500,
                 file: imgFile,
-                type: "jpeg"
+                type: "jpeg",
+                bounds: "1 1 7 7"
         )
         StringWriter w = new StringWriter()
         cmd.execute(options, readCsv("polygons.csv"), w)
         assertTrue imgFile.exists()
     }
 
-    @Test void runAsCommandLine() {
+    @Test
+    void runAsCommandLine() {
         File file = getResource("polygons.properties")
-        File imgFile = File.createTempFile("polygons_",".pdf")
-        println imgFile
+        File imgFile = File.createTempFile("polygons_", ".pdf")
         App.main([
-            "vector draw",
-            "-i", file.absolutePath,
-            "-w", "800",
-            "-h", "700",
-            "-f", imgFile.absolutePath,
-            "-t", "pdf"
+                "vector draw",
+                "-i", file.absolutePath,
+                "-w", "800",
+                "-h", "700",
+                "-f", imgFile.absolutePath,
+                "-t", "pdf"
         ] as String[])
         assertTrue imgFile.exists()
 
-        imgFile = File.createTempFile("polygons_",".svg")
-        println imgFile
+        imgFile = File.createTempFile("polygons_", ".svg")
         String output = runApp([
-            "vector draw",
-            "-w", "800",
-            "-h", "700",
-            "-f", imgFile.absolutePath,
-            "-t", "svg"
+                "vector draw",
+                "-w", "800",
+                "-h", "700",
+                "-f", imgFile.absolutePath,
+                "-t", "svg"
         ], readCsv("polygons.csv").text)
         assertTrue imgFile.exists()
     }

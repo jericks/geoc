e1c4983c2581fa439bd85c6cb7eccf9cf9608665
jericks <jared.erickson@gmail.com>
2014-12-15 19:43:23 -0800

    Add geoc vector database index create command

---
+++ src/main/groovy/org/geocommands/vector/DatabaseCreateIndexCommand.groovy
@@ -0,0 +1,66 @@
+package org.geocommands.vector
+
+import au.com.bytecode.opencsv.CSVWriter
+import geoscript.layer.Layer
+import geoscript.workspace.Database
+import geoscript.workspace.Workspace
+import groovy.sql.Sql
+import org.geocommands.Command
+import org.geocommands.Options
+import org.kohsuke.args4j.Option
+
+/**
+ * A Command to create an index of a database table
+ * @author Jared Erickson
+ */
+class DatabaseCreateIndexCommand extends Command<DatabaseCreateIndexOptions> {
+
+    @Override
+    String getName() {
+        "vector database index create"
+    }
+
+    @Override
+    String getDescription() {
+        "Create a database index"
+    }
+
+    @Override
+    DatabaseCreateIndexOptions getOptions() {
+        new DatabaseCreateIndexOptions()
+    }
+
+    @Override
+    void execute(DatabaseCreateIndexOptions options, Reader reader, Writer writer) throws Exception {
+        Workspace workspace = Util.getWorkspace(options.databaseWorkspace)
+        try {
+            if (!(workspace instanceof Database)) {
+                throw new IllegalArgumentException("Workspace must be a Database based Workspace!")
+            }
+            Database database = workspace as Database
+            database.createIndex(options.layerName, options.indexName, options.fieldNames, options.unique)
+        }
+        finally {
+            workspace.close()
+        }
+    }
+
+    static class DatabaseCreateIndexOptions extends Options {
+
+        @Option(name = "-w", aliases = "--database-workspace", usage = "The input workspace", required = true)
+        String databaseWorkspace
+
+        @Option(name = "-l", aliases = "--layer-name", usage = "The input workspace", required = true)
+        String layerName
+
+        @Option(name = "-i", aliases = "--index-name", usage = "The input workspace", required = true)
+        String indexName
+
+        @Option(name = "-f", aliases = "--field", usage = "The input workspace", required = true)
+        List<String> fieldNames
+
+        @Option(name = "-u", aliases = "--unique", usage = "The input workspace", required = false)
+        boolean unique
+    }
+
+}
---
+++ src/test/groovy/org/geocommands/vector/DatabaseCreateIndexCommandTest.groovy
@@ -0,0 +1,57 @@
+package org.geocommands.vector
+
+import geoscript.workspace.Database
+import geoscript.workspace.H2
+import org.geocommands.vector.CopyCommand.CopyOptions
+import org.geocommands.vector.DatabaseCreateIndexCommand.DatabaseCreateIndexOptions
+import org.geocommands.BaseTest
+import org.junit.Test
+import static org.junit.Assert.assertTrue
+
+/**
+ * The DatabaseCreateIndexCommand Unit Test
+ * @author Jared Erickson
+ */
+class DatabaseCreateIndexCommandTest extends BaseTest {
+
+     private File createDB() {
+         File file = createTemporaryFile("points50", ".db")
+         CopyCommand cmd = new CopyCommand()
+         CopyOptions options = new CopyOptions(
+                 inputWorkspace: getResource("points50.properties"),
+                 outputWorkspace: file,
+                 outputLayer: "points50"
+         )
+         cmd.execute(options)
+         file
+     }
+
+    @Test void execute() {
+        File dbFile = createDB()
+        DatabaseCreateIndexCommand cmd = new DatabaseCreateIndexCommand()
+        DatabaseCreateIndexOptions options = new DatabaseCreateIndexOptions(
+                databaseWorkspace: dbFile,
+                indexName: "geom_index",
+                layerName: "points50",
+                fieldNames: ["the_geom"],
+                unique: false
+        )
+        cmd.execute(options)
+        Database db = new H2(dbFile)
+        assertTrue([name:"geom_index", unique:false, attributes:["the_geom"]] in db.getIndexes("points50"))
+    }
+
+    @Test void run() {
+        File dbFile = createDB()
+        runApp([
+            "vector database index create",
+                "-w", dbFile.absolutePath,
+                "-i", "geom_index",
+                "-l", "points50",
+                "-f", "the_geom"
+        ], "")
+        Database db = new H2(dbFile)
+        assertTrue([name:"geom_index", unique:false, attributes:["the_geom"]] in db.getIndexes("points50"))
+    }
+
+}

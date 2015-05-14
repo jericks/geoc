508a228037d16af432243e83bfc9b37089f792f1
jericks <jared.erickson@gmail.com>
2015-05-13 20:13:59 -0700

    Add geoc tile vector grid

---
+++ src/main/groovy/org/geocommands/tile/VectorGridCommand.groovy
@@ -0,0 +1,102 @@
+package org.geocommands.tile
+
+import geoscript.geom.Bounds
+import geoscript.layer.Layer
+import geoscript.layer.TileCursor
+import geoscript.layer.TileLayer
+import geoscript.workspace.Memory
+import geoscript.workspace.Workspace
+import org.geocommands.vector.LayerOutCommand
+import org.geocommands.vector.LayerOutOptions
+import org.kohsuke.args4j.Option
+
+/**
+ * Create a vector grid of a tile layers cells.
+ * @author Jared Erickson
+ */
+class VectorGridCommand extends LayerOutCommand<VectorGridOptions> {
+
+    @Override
+    String getName() {
+        "tile vector grid"
+    }
+
+    @Override
+    String getDescription() {
+        "Create a vector grid of a tile layers cells."
+    }
+
+    @Override
+    VectorGridOptions getOptions() {
+        new VectorGridOptions()
+    }
+
+    @Override
+    Layer createLayer(VectorGridOptions options, Reader reader, Writer writer) throws Exception {
+        Layer layer
+        TileLayer tileLayer = TileUtil.getTileLayer(options.tileLayer, options.tileLayerName, options.type, options.pyramid)
+        try {
+            TileCursor tileCursor
+            if (options.bounds && !options.z) {
+                tileCursor = tileLayer.tiles(Bounds.fromString(options.bounds), options.width, options.height)
+            } else if (options.bounds && options.z) {
+                tileCursor = tileLayer.tiles(Bounds.fromString(options.bounds), options.z)
+            } else if (options.z && options.minX && options.minY && options.maxX && options.maxY) {
+                tileCursor = tileLayer.tiles(options.z, options.minX, options.minY, options.maxX, options.maxY)
+            } else if (options.z) {
+                tileCursor = tileLayer.tiles(options.z)
+            } else {
+                throw new IllegalArgumentException("Wrong combination of options!")
+            }
+
+            Workspace workspace
+            if (!options.outputWorkspace) {
+                workspace = new Memory()
+            } else {
+                workspace = Workspace.getWorkspace(options.outputWorkspace)
+            }
+
+            layer = tileLayer.getLayer(tileCursor,
+                    outWorkspace: workspace,
+                    outLayer: getOutputLayerName(options, "${tileLayer.name}_tiles")
+            )
+        } finally {
+            tileLayer.close()
+        }
+        layer
+    }
+
+    static class VectorGridOptions extends LayerOutOptions {
+
+        @Option(name = "-l", aliases = "--tile-layer", usage = "The tile layer", required = true)
+        String tileLayer
+
+        @Option(name = "-n", aliases = "--tile-layer-name", usage = "The tile layer name", required = false)
+        String tileLayerName
+
+        @Option(name = "-t", aliases = "--type", usage = "The type of tile layer(png, utfgrid, mvt, pbf)", required = false)
+        String type = "pbf"
+
+        @Option(name = "-p", aliases = "--pyramid", usage = "The pyramid", required = false)
+        String pyramid = "GlobalMercator"
+
+        @Option(name = "-b", aliases = "--bounds", usage = "The bounds", required = false)
+        String bounds
+
+        @Option(name = "-z", aliases = "--zoom-level", usage = "The tile zoom level", required = false)
+        Long z = null
+
+        @Option(name = "-x", aliases = "--minx", usage = "The min x or col", required = false)
+        Long minX = null
+
+        @Option(name = "-y", aliases = "--miny", usage = "The min y or row", required = false)
+        Long minY = null
+
+        @Option(name = "-c", aliases = "--maxx", usage = "The max x or col", required = false)
+        Long maxX = null
+
+        @Option(name = "-u", aliases = "--maxy", usage = "The max y or row", required = false)
+        Long maxY = null
+
+    }
+}
---
+++ src/test/groovy/org/geocommands/tile/VectorGridCommandTest.groovy
@@ -0,0 +1,69 @@
+package org.geocommands.tile
+
+import geoscript.feature.Feature
+import geoscript.layer.Layer
+import geoscript.layer.Shapefile
+import org.geocommands.BaseTest
+import org.geocommands.tile.VectorGridCommand.VectorGridOptions
+import org.junit.Test
+import static org.junit.Assert.*
+
+/**
+ * The VectorGridCommand Unit Test
+ * @author Jared Erickson
+ */
+class VectorGridCommandTest extends BaseTest {
+
+    @Test
+    void execute() {
+        File mbtilesFile = getResource("earthquakes.mbtiles")
+        File file = createTemporaryShapefile("grid")
+        VectorGridCommand cmd = new VectorGridCommand()
+        VectorGridOptions options = new VectorGridOptions(
+                tileLayer: mbtilesFile.absolutePath,
+                tileLayerName: "earthquakes",
+                z: 1,
+                outputWorkspace: file.absolutePath
+
+
+        )
+        cmd.execute(options)
+        Layer layer = new Shapefile(file)
+        assertEquals 4, layer.count
+        ["the_geom","id","z","x","y"].each { String field ->
+            assertTrue layer.schema.has(field)
+        }
+        layer.eachFeature { Feature f ->
+            assertNotNull f.geom
+            assertTrue f["id"] >= 0
+            assertTrue f["z"] == 1
+            assertTrue f["x"] >= 0
+            assertTrue f["y"] >= 0
+        }
+    }
+
+    @Test
+    void run() {
+        File mbtilesFile = getResource("earthquakes.mbtiles")
+        File file = createTemporaryShapefile("grid")
+        runApp([
+           "tile vector grid",
+                "-l", mbtilesFile.absolutePath,
+                "-n", "earthquakes",
+                "-z", 1,
+                "-o", file.absolutePath
+        ],"")
+        Layer layer = new Shapefile(file)
+        assertEquals 4, layer.count
+        ["the_geom","id","z","x","y"].each { String field ->
+            assertTrue layer.schema.has(field)
+        }
+        layer.eachFeature { Feature f ->
+            assertNotNull f.geom
+            assertTrue f["id"] >= 0
+            assertTrue f["z"] == 1
+            assertTrue f["x"] >= 0
+            assertTrue f["y"] >= 0
+        }
+    }
+}

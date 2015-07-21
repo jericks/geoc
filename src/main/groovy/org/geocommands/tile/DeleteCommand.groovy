176a1a28d41a9f411121d17dd52cb0d65faf1752
jericks <jared.erickson@gmail.com>
2015-07-20 19:01:42 -0700

    Add geoc tile delete command

---
+++ src/main/groovy/org/geocommands/tile/DeleteCommand.groovy
@@ -0,0 +1,96 @@
+package org.geocommands.tile
+
+import geoscript.geom.Bounds
+import geoscript.layer.Tile
+import geoscript.layer.TileCursor
+import geoscript.layer.TileLayer
+import org.geocommands.Command
+import org.geocommands.Options
+import org.kohsuke.args4j.Option
+
+/**
+ * Delete tiles from a tile layer
+ * @author Jared Erickson
+ */
+class DeleteCommand extends Command<DeleteOptions> {
+
+    @Override
+    String getName() {
+        "tile delete"
+    }
+
+    @Override
+    String getDescription() {
+        "Delete tiles from a tile layer"
+    }
+
+    @Override
+    DeleteOptions getOptions() {
+        new DeleteOptions()
+    }
+
+    @Override
+    void execute(DeleteOptions options, Reader reader, Writer writer) throws Exception {
+        TileLayer tileLayer = TileUtil.getTileLayer(options.tileLayer, options.tileLayerName, options.type, options.pyramid)
+        if (options.tile) {
+            List parts = options.tile.split("/")
+            long z = parts[0] as long
+            long x = parts[1] as long
+            long y = parts[2] as long
+            Tile tile = tileLayer.get(z,x,y)
+            tileLayer.delete(tile)
+        } else {
+            TileCursor tileCursor
+            if (options.bounds && !options.z) {
+                tileCursor = tileLayer.tiles(Bounds.fromString(options.bounds), options.width, options.height)
+            } else if (options.bounds && options.z) {
+                tileCursor = tileLayer.tiles(Bounds.fromString(options.bounds), options.z)
+            } else if (options.z && options.minX && options.minY && options.maxX && options.maxY) {
+                tileCursor = tileLayer.tiles(options.z, options.minX, options.minY, options.maxX, options.maxY)
+            } else if (options.z) {
+                tileCursor = tileLayer.tiles(options.z)
+            } else {
+                throw new IllegalArgumentException("Wrong combination of options for delete tiles!")
+            }
+            tileLayer.delete(tileCursor)
+        }
+    }
+
+    static class DeleteOptions extends Options {
+
+        @Option(name = "-l", aliases = "--tile-layer", usage = "The tile layer", required = true)
+        String tileLayer
+
+        @Option(name = "-n", aliases = "--tile-layer-name", usage = "The tile layer name", required = true)
+        String tileLayerName
+
+        @Option(name = "-t", aliases = "--type", usage = "The type of tile layer(png, utfgrid, mvt, pbf)", required = false)
+        String type = "png"
+
+        @Option(name = "-p", aliases = "--pyramid", usage = "The pyramid", required = false)
+        String pyramid = "GlobalMercator"
+
+        @Option(name = "-i", aliases = "--tile", usage = "The Tile Z/X/Y coordinates", required = false)
+        String tile
+
+        @Option(name = "-b", aliases = "--bounds", usage = "The bounds", required = false)
+        String bounds
+
+        @Option(name = "-z", aliases = "--zoom-level", usage = "The tile zoom level", required = false)
+        Long z = null
+
+        @Option(name = "-x", aliases = "--minx", usage = "The min x or col", required = false)
+        Long minX = null
+
+        @Option(name = "-y", aliases = "--miny", usage = "The min y or row", required = false)
+        Long minY = null
+
+        @Option(name = "-c", aliases = "--maxx", usage = "The max x or col", required = false)
+        Long maxX = null
+
+        @Option(name = "-u", aliases = "--maxy", usage = "The max y or row", required = false)
+        Long maxY = null
+
+    }
+
+}
---
+++ src/test/groovy/org/geocommands/tile/DeleteCommandTest.groovy
@@ -0,0 +1,81 @@
+package org.geocommands.tile
+
+import geoscript.layer.MBTiles
+import geoscript.layer.Tile
+import org.geocommands.BaseTest
+import org.geocommands.tile.GenerateCommand.GenerateOptions
+import org.geocommands.tile.DeleteCommand.DeleteOptions
+import org.geocommands.BaseTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import static org.junit.Assert.*
+
+/**
+ * The DeleteCommand Unit Test
+ * @author Jared Erickson
+ */
+class DeleteCommandTest extends BaseTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder()
+
+    @Test void execute() {
+        File mbtilesFile = generateMBTiles()
+        DeleteCommand cmd = new DeleteCommand()
+        DeleteOptions options = new DeleteOptions(
+                tileLayer: mbtilesFile.absolutePath,
+                tileLayerName: "earthquakes",
+                z: 1
+        )
+        cmd.execute(options)
+
+        MBTiles mbtiles = new MBTiles(mbtilesFile)
+        (0..2).each { int z ->
+            mbtiles.tiles(z).each{ Tile t ->
+                if (z == 1) {
+                    assertNull t.data
+                } else {
+                    assertNotNull t.data
+                }
+            }
+        }
+    }
+
+    @Test void run() {
+        File mbtilesFile = generateMBTiles()
+        String result = runApp([
+                "tile delete",
+                "-l", mbtilesFile.absolutePath,
+                "-n", "earthquakes",
+                "-i", "1/1/0"
+        ],"")
+        MBTiles mbtiles = new MBTiles(mbtilesFile)
+        (0..2).each { int z ->
+            mbtiles.tiles(z).each{ Tile t ->
+                if (t.z == 1 && t.x == 1 && t.y == 0) {
+                    assertNull t.data
+                } else {
+                    assertNotNull t.data
+                }
+            }
+        }
+    }
+
+    private File generateMBTiles() {
+        GenerateCommand cmd = new GenerateCommand()
+        File tileFile = temporaryFolder.newFile("earthquakes.mbtiles")
+        File layerFile = getResource("earthquakes.properties")
+        GenerateOptions options = new GenerateOptions(
+                tileLayer: tileFile.absolutePath,
+                tileLayerName: "earthquakes",
+                baseMap: layerFile,
+                startZoom: 0,
+                endZoom: 2,
+                verbose: false
+        )
+        cmd.execute(options)
+        tileFile
+    }
+
+}

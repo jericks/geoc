3f0684498db6711663f9f7d07b01b34abf9469e6
jericks <jared.erickson@gmail.com>
2016-02-22 18:14:15 -0800

    Add unit test for the tile utility class

---
+++ src/test/groovy/org/geocommands/tile/TileUtilTest.groovy
@@ -0,0 +1,28 @@
+package org.geocommands.tile
+
+import geoscript.layer.OSM
+import org.junit.Test
+import static org.junit.Assert.assertEquals
+
+/**
+ * The TileUtil Unit Test.
+ * @author Jared Erickson
+ */
+class TileUtilTest {
+
+    @Test void getOSMImageTileLayer() {
+        OSM osm = TileUtil.getOSMImageTileLayer("stamen-toner")
+        assertEquals("Stamen Toner", osm.name)
+        osm = TileUtil.getOSMImageTileLayer("stamen-toner-lite")
+        assertEquals("Stamen Toner Lite", osm.name)
+        osm = TileUtil.getOSMImageTileLayer("stamen-watercolor")
+        assertEquals("Stamen Watercolor", osm.name)
+        osm = TileUtil.getOSMImageTileLayer("mapquest-street")
+        assertEquals("MapQuest Street Map", osm.name)
+        osm = TileUtil.getOSMImageTileLayer("mapquest-satellite")
+        assertEquals("MapQuest Satellite Map", osm.name)
+        osm = TileUtil.getOSMImageTileLayer("osm")
+        assertEquals("OSM", osm.name)
+    }
+
+}

5599fedd5acdaec087c8068a8a9e82e41cce2d53
jericks <jared.erickson@gmail.com>
2016-02-29 19:59:52 -0800

    Start adding bash examples

---
+++ examples/vector_random.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+geoc vector randompoints -g -180,-90,180,90 -n 100 > points.csv
+
+cat points.csv | geoc vector defaultstyle --color navy -o 0.75 > points.sld
+
+cat points.csv | geoc vector defaultstyle --color navy -o 0.75 > points.sld
+
+geoc map draw -f vector_random.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" -l "layertype=layer file=points.csv style=points.sld"
+

cf4d1c902d9a552671d0a554a60931ae9adb86a9
jericks <jared.erickson@gmail.com>
2016-02-29 20:01:18 -0800

    Format vector_random example script

--- examples/vector_random.sh
+++ examples/vector_random.sh
@@ -5,5 +5,7 @@ cat points.csv | geoc vector defaultstyle --color navy -o 0.75 > points.sld
 
 cat points.csv | geoc vector defaultstyle --color navy -o 0.75 > points.sld
 
-geoc map draw -f vector_random.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" -l "layertype=layer file=points.csv style=points.sld"
+geoc map draw -f vector_random.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
+    -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \
+    -l "layertype=layer file=points.csv style=points.sld"
 

33bb7265fb9a4704463a913822155fd25849d413
jericks <jared.erickson@gmail.com>
2016-03-01 19:43:39 -0800

    Add geoc vector centroid and interiorpoint examples

---
+++ examples/vector_centroid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+geoc vector centroid -i naturalearth.gpkg -l countries -o countries_centroids.shp
+
+geoc vector defaultstyle --color navy -o 0.75 -i countries_centroids.shp > countries_centroids.sld
+
+geoc map draw -f vector_centroid.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
+    -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \
+    -l "layertype=layer file=countries_centroids.shp style=countries_centroids.sld"
+
---
+++ examples/vector_interiorpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+geoc vector interiorpoint -i naturalearth.gpkg -l countries -o countries_interiorpoints.shp
+
+geoc vector defaultstyle --color navy -o 0.75 -i countries_interiorpoints.shp > countries_interiorpoints.sld
+
+geoc map draw -f vector_interiorpoint.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
+    -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \
+    -l "layertype=layer file=countries_interiorpoints.shp style=countries_interiorpoints.sld"
+

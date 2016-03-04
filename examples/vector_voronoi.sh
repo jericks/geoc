6ba83411a6f601b3b74603dc33b9c44f54daf150
jericks <jared.erickson@gmail.com>
2016-03-03 19:57:57 -0800

    Add vector voronoi example

---
+++ examples/vector_voronoi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+geoc vector centroid -i naturalearth.gpkg -l countries | geoc vector voronoi -o countries_voronoi.shp
+
+geoc vector defaultstyle --color navy -o 0.15 -i countries_voronoi.shp > countries_voronoi.sld
+
+geoc map draw -f vector_voronoi.png -b "-180,-90,180,90" \
+    -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
+    -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \
+    -l "layertype=layer file=countries_voronoi.shp layername=countries_voronoi style=countries_voronoi.sld"
+

3bc0609042267fbf856fed19966107440b1975b3
jericks <jared.erickson@gmail.com>
2016-04-13 19:10:45 -0700

    Improve vector fields example with area, length, and updating

--- examples/vector_fields.sh
+++ examples/vector_fields.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm points.db.*
+
 echo "Generating 10 random points:"
 geoc vector randompoints -g -180,-90,180,90 -n 10 -o "type=h2 file=points.db" -r points
 geoc vector schema -i "type=h2 file=points.db" -l points -p
@@ -15,4 +17,21 @@ echo "Add LABEL field:"
 geoc vector addfields -i "type=h2 file=points.db" -l points_id_xy -o "type=h2 file=points.db" -r points_id_xy_flds -f label=string -f value=double
 geoc vector schema -i "type=h2 file=points.db" -l points_id_xy_flds -p
 
+echo "Update values:"
+geoc vector updatefield -i "type=h2 file=points.db" -l points_id_xy_flds -d label -v "'point label'"
+geoc vector updatefield -i "type=h2 file=points.db" -l points_id_xy_flds -d value -v "id * 10"
 geoc vector copy -i "type=h2 file=points.db" -l points_id_xy_flds
+
+
+echo "Create Polygons:"
+geoc vector buffer -d 10 -i "type=h2 file=points.db" -l points -o "type=h2 file=points.db" -r polys
+geoc vector schema -i "type=h2 file=points.db" -l polys -p
+
+echo "Add Area field:"
+geoc vector addareafield -i "type=h2 file=points.db" -l polys -o "type=h2 file=points.db" -r polys_area
+geoc vector schema -i "type=h2 file=points.db" -l polys_area -p
+
+echo "Add Length field:"
+geoc vector addlengthfield -i "type=h2 file=points.db" -l polys_area -o "type=h2 file=points.db" -r polys_area_len
+geoc vector schema -i "type=h2 file=points.db" -l polys_area_ln -p
+geoc vector copy -i "type=h2 file=points.db" -l polys_area_len

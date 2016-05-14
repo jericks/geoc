58167a88896e6598035d219b632aa4269040f2d9
jericks <jared.erickson@gmail.com>
2016-05-13 19:36:57 -0700

    Add another vector gradient example

--- examples/vector_gradientstyle.sh
+++ examples/vector_gradientstyle.sh
@@ -3,3 +3,8 @@ geoc vector gradientstyle -i naturalearth.gpkg -l countries -f PEOPLE -n 6 -c gr
 
 geoc map draw -f vector_gradient_greens.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
     -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries_gradient_green.sld"
+
+geoc vector gradientstyle -i naturalearth.gpkg -l countries -f PEOPLE -n 8 -c reds > countries_gradient_red.sld
+
+geoc map draw -f vector_gradient_reds.png -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
+    -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries_gradient_red.sld"

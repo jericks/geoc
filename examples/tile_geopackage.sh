c0680f6ca9119a085fb81bb7e1db4b0dbf430a7b
jericks <jared.erickson@gmail.com>
2016-06-13 19:32:49 -0700

    Add example of geopackage containing two tile sets

--- examples/tile_geopackage.sh
+++ examples/tile_geopackage.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 rm tiles.gpkg
 
-geoc tile generate -l "type=geopackage file=tiles.gpkg name=world pyramid=geodetic" \
+# Geodetic
+geoc tile generate -l "type=geopackage file=tiles.gpkg name=world_geodetic pyramid=geodetic" \
     -m "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
     -m "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \
     -s 0 \
-    -e 4 \
+    -e 3 \
     -v
 
-geoc tile pyramid -l "type=geopackage file=tiles.gpkg name=world" -o json
+geoc tile pyramid -l "type=geopackage file=tiles.gpkg name=world_geodetic" -o json
 
-geoc tile stitch raster -l "type=geopackage file=tiles.gpkg name=world" -o countries_2.png -z 2
+geoc tile stitch raster -l "type=geopackage file=tiles.gpkg name=world_geodetic" -o world_geodetic_2.png -z 2
+
+# Mercator
+geoc tile generate -l "type=geopackage file=tiles.gpkg name=world_mercator pyramid=mercator" \
+    -m "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
+    -m "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \
+    -s 0 \
+    -e 3 \
+    -v
+
+geoc tile pyramid -l "type=geopackage file=tiles.gpkg name=world_mercator" -o json
+
+geoc tile stitch raster -l "type=geopackage file=tiles.gpkg name=world_mercator" -o world_mercator_2.png -z 2

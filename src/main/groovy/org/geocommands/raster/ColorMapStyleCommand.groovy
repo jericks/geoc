1a9abe9058682d540645cce8c5d816b3908e861a
jericks <jared.erickson@gmail.com>
2016-07-17 13:19:52 -0700

    Add types

--- src/main/groovy/org/geocommands/raster/ColorMapStyleCommand.groovy
+++ src/main/groovy/org/geocommands/raster/ColorMapStyleCommand.groovy
@@ -28,8 +28,8 @@ class ColorMapStyleCommand extends Command<ColorMapStyleOptions> {
 
     @Override
     void execute(ColorMapStyleOptions options, Reader reader, Writer writer) throws Exception {
-        ColorMap style = new ColorMap(options.values.collect { v ->
-            def parts = v.split("=")
+        ColorMap style = new ColorMap(options.values.collect { String v ->
+            List parts = v.split("=") as List
             [quantity: parts[0], color: new Color(parts[1]).hex]
         }, options.type, options.extended)
         style.opacity = options.opacity

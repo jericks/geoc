7e01518bec90d9dd8943ac68eca354848996af76
jericks <jared.erickson@gmail.com>
2017-10-06 17:45:12 -0700

    Add title and source to the map cubes example

--- examples/map_cube.sh
+++ examples/map_cube.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-geoc map cube -f map_cube.png \
+geoc map cube -f map_cube.png -t -o -i 'Earth' -c 'Natural Earth' \
     -l "layertype=layer file=naturalearth.gpkg layername=ocean style=ocean.sld" \
     -l "layertype=layer file=naturalearth.gpkg layername=countries style=countries.sld" \

2b9504bbea277aadc10c49c64b66bba90587c532
Jared Erickson <jared.erickson@gmail.com>
2023-09-07 16:16:49 -0700

    Add raster add recipes.

--- src/test/groovy/org/geocommands/doc/RasterTest.groovy
+++ src/test/groovy/org/geocommands/doc/RasterTest.groovy
@@ -38,6 +38,63 @@ class RasterTest extends DocTest {
     }
 
     @Test
+    void addConstant() {
+        String getValue1Command = "geoc raster get value -i src/test/resources/pc.tif -x -121.799927 -y 46.867703"
+        String getValue1Result = runApp(getValue1Command)
+        writeTextFile("geoc_raster_add_constant_value1_command", getValue1Command)
+        writeTextFile("geoc_raster_add_constant_value1_result", getValue1Result)
+
+        String command = "geoc raster add constant -i src/test/resources/pc.tif -v 100 -o target/pc_add.tif"
+        String result = runApp(command, "")
+        writeTextFile("geoc_raster_add_constant_command", command)
+        writeTextFile("geoc_raster_add_constant_command_output", result)
+
+        Raster raster = Format.getFormat(new File("target/pc_add.tif")).read()
+        raster.style = new ColorMap([
+                [color: "#9fd182", quantity:25],
+                [color: "#3e7f3c", quantity:470],
+                [color: "#133912", quantity:920],
+                [color: "#08306b", quantity:1370],
+                [color: "#fffff5", quantity:1820],
+        ])
+        draw("geoc_raster_add_constant_command_raster", [raster])
+
+        String getValue2Command = "geoc raster get value -i target/pc_add.tif -x -121.799927 -y 46.867703"
+        String getValue2Result = runApp(getValue2Command)
+        writeTextFile("geoc_raster_add_constant_value2_command", getValue2Command)
+        writeTextFile("geoc_raster_add_constant_value2_result", getValue2Result)
+    }
+
+    @Test
+    void add() {
+        String command = "geoc raster add -i src/test/resources/low.tif -k src/test/resources/high.tif -o target/lowPlusHigh.tif"
+        String result = runApp(command, "")
+        writeTextFile("geoc_raster_add_command", command)
+        writeTextFile("geoc_raster_add_command_output", result)
+
+        Style rasterStyle = new ColorMap(1, 50, "MutedTerrain", 20)
+        Style vectorStyle = new Stroke("black",1) + new Label("value")
+
+        Raster lowRaster = Format.getFormat(new File("src/test/resources/low.tif")).read()
+        Raster highRaster = Format.getFormat(new File("src/test/resources/high.tif")).read()
+        Raster lowPlusHighRaster = Format.getFormat(new File("target/lowPlusHigh.tif")).read()
+
+        lowRaster.style = rasterStyle
+        highRaster.style = rasterStyle
+        lowPlusHighRaster.style = rasterStyle
+
+        Closure createLayer = { Raster raster, Style style ->
+            Layer layer = raster.polygonLayer
+            layer.style = style
+            layer
+        }
+
+        draw("geoc_raster_add_command_low",  [lowRaster, createLayer(lowRaster, vectorStyle)])
+        draw("geoc_raster_add_command_high", [highRaster, createLayer(highRaster, vectorStyle)])
+        draw("geoc_raster_add_command_add",  [lowPlusHighRaster, createLayer(lowPlusHighRaster, vectorStyle)])
+    }
+
+    @Test
     void crop() {
         String command = "geoc raster crop -i src/test/resources/earth.tif -b -160.927734,6.751896,-34.716797,57.279043 -o target/earth_cropped.tif"
         String result = runApp(command, "")
